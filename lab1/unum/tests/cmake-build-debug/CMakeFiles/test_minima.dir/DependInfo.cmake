188d7085602c5a1b267eba2446ee012592743da6
Alek Salikhzyanov <a.salikhzyanov@innopolis.ru>
2019-09-07 00:24:50 +0300

    HW1. Alek Salikhzyanov. DS2

---
+++ lab1/unum/tests/cmake-build-debug/CMakeFiles/test_minima.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alec/CLionProjects/NumModelingHW1/tmp/inno_nummod_fall_2019/lab1/unum/tests/test_minima.c" "/home/alec/CLionProjects/NumModelingHW1/tmp/inno_nummod_fall_2019/lab1/unum/tests/cmake-build-debug/CMakeFiles/test_minima.dir/test_minima.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
