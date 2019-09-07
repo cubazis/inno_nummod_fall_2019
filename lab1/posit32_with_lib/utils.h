f4c17173a670a0ba21bd7a8d7a138da17eb0d20f
Ansat.Abirov <Ansat.Abirov@acronis.com>
2019-09-07 21:27:21 +0400

    external library added

---
+++ lab1/posit32_with_lib/utils.h
@@ -0,0 +1,26 @@
+#define CLZ(n) ((n) == 0 ? 8 * sizeof(n) : __builtin_clz(n))
+
+#define _uint64 unsigned long long
+#define bool short
+#define _uint32 unsigned int
+
+#define LSHIFT(bits, shift) \
+    ((shift) >= (int)(8 * sizeof(bits)) ? 0 : (bits) << (shift))
+
+#define RSHIFT(bits, shift) \
+    ((shift) >= (int)(8 * sizeof(bits)) ? 0 : (bits) >> (shift))
+
+#define POW2(n) \
+    (LSHIFT(1, (n)))
+
+#define FLOORDIV(a, b) \
+    ((a) / (b) - ((a) % (b) < 0))
+
+#define HIDDEN_BIT(a) \
+    ((a >> 1) | MSB)
+
+#define MIN(a, b) \
+    ((a) < (b) ? (a) : (b))
+
+#define MAX(a, b) \
+    ((a) > (b) ? (a) : (b))
