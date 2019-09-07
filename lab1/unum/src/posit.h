49e16a2bc4239bd2ab3aa9e5471015bcaeb8309f
Timur <minteemer@gmail.com>
2019-09-07 21:46:08 +0300

    lab1; Timur Valiev

--- lab1/unum/src/posit.h
+++ lab1/unum/src/posit.h
@@ -5,10 +5,48 @@
 #ifndef UNUM_POSIT_H
 #define UNUM_POSIT_H
 
-typedef struct _posit32 {
-	/** implement your code **/
 
+#define _uint64 unsigned long long
+
+#define _uint32 unsigned int
+
+#define bool short
+
+
+#define FLOOR_DIV(a, b) ((a) / (b) - ((a) % (b) < 0))
+
+#define HIDDEN_BIT(a) ((a >> 1) | MSB)
+
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+#define RIGHT_SHIFT(bits, shift) ((shift) >= (int)(8 * sizeof(bits)) ? 0 : (bits) >> (shift))
+
+#define LEFT_SHIFT(bits, shift) ((shift) >= (int)(8 * sizeof(bits)) ? 0 : (bits) << (shift))
+
+#define POW2(n) (LEFT_SHIFT(1, (n)))
+
+#define CLZ(n) ((n) == 0 ? 8 * sizeof(n) : __builtin_clz(n))
+
+
+typedef struct _posit32 {
+    unsigned int bits;
 } posit;
+
+posit fromDouble(double a);
+
+double toDouble(posit a);
+
+posit add(posit a, posit b);
+
+posit subtract(posit a, posit b);
+
+posit multiply(posit a, posit b);
+
+posit power(posit a, posit b);
+
+posit negate(posit a);
 
 int test_function(void);
 
