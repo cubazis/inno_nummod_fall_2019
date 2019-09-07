5b1d40f462ea4ecbc764a63bf7789b215b12dde0
Denis <den1503777@gmail.com>
2019-09-07 14:58:50 +0300

    resolving problem after merging with master

--- lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/DependInfo.cmake
+++ lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/DependInfo.cmake
@@ -1,16 +1,29 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "C"
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/cubazis/Projects/inno_nummod_fall_2019/lab1/unum/src/posit.c" "/home/cubazis/Projects/inno_nummod_fall_2019/lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/src/posit.c.o"
+  "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/src/op1.c" "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/src/op1.c.o"
+  "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/src/op2.c" "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/src/op2.c.o"
+  "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/src/pack.c" "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/src/pack.c.o"
+  "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/src/util.c" "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/src/util.c.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_C_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../src"
+  "/usr/local/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/src/posit.cpp" "/Users/denisrangulov/Обучение/Numerical Modeling/inno_nummod_fall_2019/lab1/unum/cmake-build-debug/CMakeFiles/unum.dir/src/posit.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
   )
 
 # Targets to which this target links.
