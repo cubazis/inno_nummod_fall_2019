35bfedef04f6aab7b96a27dc61ade73b83b2d24a
InfoLight <asddinar999@mail.ru>
2019-09-07 22:23:29 +0300

    Update posit.h

--- lab1/unum/src/posit.h
+++ lab1/unum/src/posit.h
@@ -1,14 +1,25 @@
-//
-// Created by cubazis on 18.08.2019.
-//
-
 #ifndef UNUM_POSIT_H
 #define UNUM_POSIT_H
 
 typedef struct _posit32 {
-	/** implement your code **/
-
+	unsigned int bits;
 } posit;
+
+posit from_double(double a);
+
+double to_double(posit a);
+
+posit add(posit a, posit b);
+
+posit subtract(posit a, posit b);
+
+posit multiply(posit a, posit b);
+
+posit divide(posit a, posit b);
+
+posit power(posit a, posit b);
+
+posit negate(posit a);
 
 int test_function(void);
 
