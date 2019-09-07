bc8b8b3bbf4f1ad72d92882bc9b5a3756d4cef5e
Hidancloud <33417319+Hidancloud@users.noreply.github.com>
2019-09-07 21:31:56 +0300

    Revert "HW 1; Pinigin Arseny; Ceeeeeeb"

--- operations.h
+++
@@ -1,7 +0,0 @@
-#include "packing_posit.h"
-
-struct unpacked_t op2_add(struct unpacked_t a, struct unpacked_t b);
-struct unpacked_t op2_sub(struct unpacked_t a, struct unpacked_t b);
-struct unpacked_t op2_mul(struct unpacked_t a, struct unpacked_t b);
-struct unpacked_t op2_div(struct unpacked_t a, struct unpacked_t b);
-struct unpacked_t posit_sqrt(struct unpacked_t a);
