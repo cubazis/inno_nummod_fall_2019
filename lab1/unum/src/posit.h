183acaa0e96b4adbef43c132b3a68168ab72e211
Ilya Dubovitsky <ilya.dubovitsky@gmail.com>
2019-09-07 20:59:07 +0300

    posit redone; tests passed

---
+++ lab1/unum/src/posit.h
@@ -0,0 +1,29 @@
+#ifndef UNUM_POSIT_H
+#define UNUM_POSIT_H
+
+typedef struct _posit32 
+{
+    uint32_t bits;
+} 
+posit;	
+
+posit fromDouble(double a);
+
+double toDouble(posit a);
+
+posit add(posit a, posit b);
+
+posit sub(posit a, posit b);
+
+posit multiply(posit a, posit b);
+
+posit divide(posit a, posit b);
+
+posit power(posit a, posit b);
+
+posit negate(posit a);
+
+posit p_abs(posit ap);
+
+short is_less(posit rhs, posit lhs);
+#endif 
