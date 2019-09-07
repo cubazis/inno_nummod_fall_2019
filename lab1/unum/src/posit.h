00f6de01b1d8c6503212480ab01394b892ed7c27
ipodlipnova <i.podlipnova@innopolis.ru>
2019-09-07 19:08:50 +0300

    Homework1

--- lab1/unum/src/posit.h
+++ lab1/unum/src/posit.h
@@ -5,10 +5,52 @@
 #ifndef UNUM_POSIT_H
 #define UNUM_POSIT_H
 
-typedef struct _posit32 {
-	/** implement your code **/
+#define CLZ(n) ((n) == 0 ? 8 * sizeof(n) : __builtin_clz(n))
 
+#define _uint64 unsigned long long
+#define bool short
+#define _uint32 unsigned int
+
+#define LSHIFT(bits, shift) \
+    ((shift) >= (int)(8 * sizeof(bits)) ? 0 : (bits) << (shift))
+
+#define RSHIFT(bits, shift) \
+    ((shift) >= (int)(8 * sizeof(bits)) ? 0 : (bits) >> (shift))
+
+#define POW2(n) \
+    (LSHIFT(1, (n)))
+
+#define FLOORDIV(a, b) \
+    ((a) / (b) - ((a) % (b) < 0))
+
+#define HIDDEN_BIT(a) \
+    ((a >> 1) | MSB)
+
+#define MIN(a, b) \
+    ((a) < (b) ? (a) : (b))
+
+#define MAX(a, b) \
+    ((a) > (b) ? (a) : (b))
+
+typedef struct _posit32 {
+    unsigned int bits;
 } posit;
+
+posit fromDouble(double a);
+
+double toDouble(posit a);
+
+posit add(posit a, posit b);
+
+posit sub(posit a, posit b);
+
+posit multiply(posit a, posit b);
+
+//posit divide(posit a, posit b);
+
+posit power(posit a, posit b);
+
+posit negate(posit a);
 
 int test_function(void);
 
--- lab1/unum/tests/test_dot_product_posit.c
+++ lab1/unum/tests/test_dot_product_posit.c
@@ -13,59 +13,81 @@ struct vector {
 	posit x6;
 };
 
-struct vector* x;
-struct vector* y;
+struct vector *x;
+struct vector *y;
 
-posit bs[5];
-posit res;
-posit eps;
-
-void init_v1(struct vector *v, posit a)
-{
-
+void init_v1(struct vector *v, posit a) {
+	v->x1 = power(fromDouble(10), a);
+	v->x2 = fromDouble(1223);
+	v->x3 = power(fromDouble(10), sub(a, fromDouble(1)));
+	v->x4 = power(fromDouble(10), sub(a, fromDouble(2)));
+	v->x5 = fromDouble(3);
+	v->x6 = negate(power(fromDouble(10), sub(a, fromDouble(5))));
 }
 
-void init_v2(struct vector *v, posit b)
-{
-
+void init_v2(struct vector *v, posit b) {
+	v->x1 = power(fromDouble(10), b);
+	v->x2 = fromDouble(2);
+	v->x3 = negate(power(fromDouble(10), add(b, fromDouble(1))));
+	v->x4 = power(fromDouble(10), b);
+	v->x5 = fromDouble(2111);
+	v->x6 = power(fromDouble(10), add(b, fromDouble(3)));
 }
 
-posit dot(struct vector *v1, struct vector *v2)
-{
-
+posit dot(struct vector *v1, struct vector *v2) {
+	posit arr[6] = {multiply(v1->x1, v2->x1), multiply(v1->x2, v2->x2), multiply(v1->x3, v2->x3),
+					multiply(v1->x4, v2->x4), multiply(v1->x5, v2->x5), multiply(v1->x6, v2->x6)};
+	posit a = add(arr[0], arr[2]);
+	posit b = add(arr[3], arr[5]);
+	posit c = add(arr[1], arr[4]);
+	return add(add(a, b), c);
 }
 
-void print_vec(struct vector *v)
-{
-
+void print_vec(struct vector *v) {
+	printf("{%lf, %lf, %lf, %lf, %lf, %lf}\n", toDouble(v->x1), toDouble(v->x2), toDouble(v->x3),
+		   toDouble(v->x4), toDouble(v->x5), toDouble(v->x6));
 }
 
-START_TEST(test_a5)
-{
-	printf("test_double_a5\n");
-}
+START_TEST(test_a5) {
+		posit bs[5] = {fromDouble(5), fromDouble(8), fromDouble(12), fromDouble(15), fromDouble(20)};
+		posit res = fromDouble(8779);
+		posit eps = fromDouble(10e-8);
+		printf("test_posit_a5\n");
+		init_v1(x, fromDouble(5));
+		for (int i = 0; i < 5; ++i) {
+			init_v2(y, bs[i]);
+			printf("%lf\n", toDouble(dot(x, y)));
+			ck_assert(fabs(toDouble(sub(res, dot(x, y)))) < toDouble(eps));
+		}
+	}
+
 END_TEST
 
-START_TEST(test_a10)
-{
-	printf("test_double_a10\n");
-}
+START_TEST(test_a10) {
+		posit bs[5] = {fromDouble(5), fromDouble(8), fromDouble(12), fromDouble(15), fromDouble(20)};
+		posit res = fromDouble(8779);
+		posit eps = fromDouble(10e-8);
+		printf("test_posit_a10\n");
+		init_v1(x, fromDouble(10));
+		for (int i = 0; i < 5; ++i) {
+			init_v2(y, bs[i]);
+			printf("%lf\n", toDouble(dot(x, y)));
+			ck_assert(fabs(toDouble(sub(res, dot(x, y)))) < toDouble(eps));
+		}}
+
 END_TEST
 
-void setup(void)
-{
+void setup(void) {
 	x = calloc(1, sizeof(struct vector));
 	y = calloc(1, sizeof(struct vector));
 }
 
-void teardown(void)
-{
+void teardown(void) {
 	free(x);
 	free(y);
 }
 
-Suite* str_suite (void)
-{
+Suite *str_suite(void) {
 	Suite *suite = suite_create("posit");
 	TCase *tcase = tcase_create("test_dot_product_posit");
 	tcase_add_checked_fixture(tcase, setup, teardown);
@@ -79,7 +101,7 @@ Suite* str_suite (void)
 	return suite;
 }
 
-int main (void) {
+int main(void) {
 	int number_failed;
 	Suite *suite = str_suite();
 	SRunner *runner = srunner_create(suite);
