6d5a28aec6a9992513943cbfe0e75c7f7233ad2d
ed <ed@ubuntu.ubuntu-domain>
2019-09-07 12:42:52 +0300

    Задачу сделал
    Сделал задачу

---
+++ lab1/tests/cmake-build-debug/CMakeFiles/test_dot_product_posit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_dot_product_posit.dir/test_dot_product_posit.o"
+  "test_dot_product_posit.pdb"
+  "test_dot_product_posit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_dot_product_posit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab1/tests/cmake-build-debug/CMakeFiles/test_minima_posit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_minima_posit.dir/test_minima_posit.o"
+  "test_minima_posit.pdb"
+  "test_minima_posit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_minima_posit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
