fa0467719cd877a4dc4cff32140c9fa17a2386c5
mshvdkmv <evmiev@icloud.com>
2019-09-07 13:21:02 +0300

    lab1; Mikhail Evdokimov; VSPOMNILI? ZAFIXIROVALI? VOPROS NA ZDACHU.

--- lab1/unum/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cubazis/Projects/inno_nummod_fall_2019/lab1/unum
-# Build directory: /home/cubazis/Projects/inno_nummod_fall_2019/lab1/unum/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
