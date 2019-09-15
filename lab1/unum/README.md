b78c07aefa211529f22e58cd0371776260666e5b
kamillaborodina <kmllbrdn@gmail.com>
2019-09-15 17:18:02 +0300

    HW

---
+++ lab1/unum/README.md
@@ -0,0 +1,15 @@
+### Зависимости
+* CMake
+* Make
+* GCC
+* Boost (C++ Library)
+
+### Запуск тестов
+Из папки **unum**
+```
+mkdir build
+cd build
+cmake ..
+make
+make test
+```
---
+++ lab1/unum/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/Kamilla/Documents/GitHub/inno_nummod_fall_2019/lab1/unum
+# Build directory: /cygdrive/c/Users/Kamilla/Documents/GitHub/inno_nummod_fall_2019/lab1/unum/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_dot_product_posit "test_dot_product_posit")
+set_tests_properties(test_dot_product_posit PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/c/Users/Kamilla/Documents/GitHub/inno_nummod_fall_2019/lab1/unum/CMakeLists.txt;20;add_test;/cygdrive/c/Users/Kamilla/Documents/GitHub/inno_nummod_fall_2019/lab1/unum/CMakeLists.txt;0;")
