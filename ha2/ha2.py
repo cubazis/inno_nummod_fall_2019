f23fc522b404ea5124276966a9c9f4618bf79697
Vladislav Kuleykin <kzvdar42@gmail.com>
2019-09-25 04:10:35 +0300

    Update ha2.py

--- ha2/ha2.py
+++ ha2/ha2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def cubic_spline(x, Y):
     """
@@ -11,9 +12,7 @@ def cubic_spline(x, Y):
     dlt_x = x[1:] - x[:-1]
     dlt_Y = Y[:, 1:] - Y[:, :-1]
     
-    alphas = dlt_x[:-1].copy()
     betas  = 2 * (dlt_x[:-1] + dlt_x[1:])
-    gammas = dlt_x[1:].copy()
     deltas = 6 * (dlt_Y[:, 1:] / dlt_x[1:] - dlt_Y[:, :-1] / dlt_x[:-1])
     
     N = deltas.shape[1]
@@ -21,29 +20,31 @@ def cubic_spline(x, Y):
     # Direct pass
     P = np.zeros((Y.shape[0], N), dtype=np.float64)
     Q = np.zeros((Y.shape[0], N), dtype=np.float64)
-    P[:, 0] = - gammas[0] / betas[0]
+    P[:, 0] = - dlt_x[1] / betas[0]
     Q[:, 0] = deltas[:, 0] / betas[0]
     for i in range(1, N):
-        common_part = betas[i] + alphas[i] * P[:, i - 1]
-        Q[:, i] = ((deltas[:, i] - alphas[i] * Q[:, i - 1]) / common_part)
-        P[:, i] = - gammas[i] / common_part
+        common_part = betas[i] + dlt_x[i] * P[:, i - 1]
+        Q[:, i] = ((deltas[:, i] - dlt_x[i] * Q[:, i - 1]) / common_part)
+        P[:, i] = - dlt_x[i + 1] / common_part
+    del betas, deltas
     
     # Reverse pass
     m = np.zeros((Y.shape[0], x.shape[0]))
     m[:, -2] = Q[:, -1]
     for i in range(N - 2, -1, -1):
         m[:, i + 1] = P[:, i] * m[:, i + 2] + Q[:, i]
+    del P, Q
     
     # Calculate final coefficients
-    a = np.zeros((Y.shape[0], 4, x.shape[0] - 1), dtype=np.float64)
+    A = np.zeros((Y.shape[0], 4, x.shape[0] - 1), dtype=np.float64)
     
     common_part = (m[:, 1:] - m[:, :-1]) / 6
-    a[:, 0, :] = Y[:, :-1]
-    a[:, 1, :] = dlt_Y / dlt_x - dlt_x * m[:, :-1] / 2 - dlt_x * common_part    
-    a[:, 2, :] = m[:, :-1] / 2
-    a[:, 3, :] = common_part / dlt_x
-        
-    return a
+    A[:, 0, :] = Y[:, :-1]
+    A[:, 1, :] = dlt_Y / dlt_x - dlt_x * m[:, :-1] / 2 - dlt_x * common_part    
+    A[:, 2, :] = m[:, :-1] / 2
+    A[:, 3, :] = common_part / dlt_x
+    
+    return A
 
 def interpolate(P, x, A):
     """
@@ -54,20 +55,19 @@ def interpolate(P, x, A):
     :param A: numpy array of parameters for different cubic splines
     :return: Interpolation result
     """ 
-    # check if all the points are inside the covered region.
+    # # check if all the points are inside the covered region.
     # if any(P > x[-1]) or any(P < x[0]):
         # raise ValueError('point outside the defined region')
-    # find the needed spline
-    p_index = 0
-    res = np.zeros((P.shape[0], A.shape[0]), dtype=np.float64)
-    for i, pi in enumerate(P):
-        while x[p_index + 1] < pi: p_index += 1
-        dlt_x = pi - x[p_index]
-        res[i, :] = ( A[:, 0, p_index]
-                    + A[:, 1, p_index] * dlt_x
-                    + A[:, 2, p_index] * dlt_x ** 2
-                    + A[:, 3, p_index] * dlt_x ** 3)
-    return res
+    
+    # find the needed splines for each point
+    inds = np.searchsorted(x, P, side='right') - 1
+    inds = np.where(inds >= x.shape[0] - 1, inds - 1, inds)
+    
+    dlt_x = P - x[inds]
+    return ( A[:, 0, inds]
+           + A[:, 1, inds] * dlt_x
+           + A[:, 2, inds] * dlt_x ** 2
+           + A[:, 3, inds] * dlt_x ** 3)
             
 def str_to_floats(s, sep=None):
     """
@@ -77,11 +77,14 @@ def str_to_floats(s, sep=None):
     :param sep: separator between float values in input string
     :return: numpy array of floats
     """
-    splitted = s.strip().split(sep)
-    res = np.zeros(len(splitted), dtype=np.float64)
-    for i, si in enumerate(splitted):
-        res[i] = si
-    return res
+    return np.array(list(map(float, s.strip().split(sep))), dtype=np.float64)
+    
+    # splitted = s.strip().split(sep)
+    # res = np.zeros(len(splitted), dtype=np.float64)
+    # for i, si in enumerate(splitted):
+        # res[i] = si
+    # return res
+    
 #     return np.fromstring(str, dtype=float, sep=sep)
 #     return np.array(list(float(x) for x in str.split(sep)))
 
@@ -104,13 +107,14 @@ if __name__ == "__main__":
         # raise ValueError('Wrong amount of arguments')
     
     # I'm doing this with a step, just because of the memory limitation
-    step = 250
+    step = 350
     for i in range(0, Y.shape[0], step):
         Ai = cubic_spline(x, Y[i:i+step])
-        print('\n'.join(map(lambda x: ' '.join(map(str, x)) # lambda x: '{:.16}'.format(x)
-                           , interpolate(points, x, Ai).T
-                           )
-                       ))
+        np.savetxt( sys.stdout.buffer
+                  , interpolate(points, x, Ai)
+                  , newline=' '
+                  , fmt="%.10f"
+                  )
     pass
     
     # -------------------- For 'input.txt'/'output.txt' I/O --------------------
@@ -132,11 +136,11 @@ if __name__ == "__main__":
     
     # with open('output.txt', 'w') as file:
         # # I'm doing this with a step, just because of the memory limitation
-        # step = 250
+        # step = 350
         # for i in range(0, Y.shape[0], step):
             # Ai = cubic_spline(x, Y[i:i+step])
             # file.write('\n'.join(map(lambda x: ' '.join(map(str, x)) # lambda x: '{:.16}'.format(x)
-                                     # , interpolate(points, x, Ai).T
+                                     # , interpolate(points, x, Ai)
                                      # )
                                  # ))
             # if i < Y.shape[0]: file.write('\n')
